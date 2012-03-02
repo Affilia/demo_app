3a675599e95d26198c75f0d038d4839894c19b8a
Boris Furman <bfurman@affilia.com>
2012-03-02 17:17:45 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'a5c1cf7581bc665b5d088132213d261e95cbe5f44e6d6cd62c80aa7feb31e7e46a252fcd4250949773baf65c2d128f91242180e4dbddaf93a470acc208659cc9'
